ef4812d9a69c32ef758150ac97dc735693e7eac1
Karina Pereira de Lemos <karinalemos@id.uff.br>
2022-10-09 19:11:59 -0300

    inserindo main no sequencial

--- utils.c
+++ utils.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include "vetor.c"
+#include <time.h>
+
 void troca(double *vetor, int i, int j){
 	int temp;
 	temp = vetor[i];
@@ -15,4 +19,23 @@ void bubble_sort(double *vetor, int n){
             }
         }
     }
+}
+
+int main (){
+
+	#define NUM 10
+
+	double *vetor_elementos; 
+	vetor_elementos = le_vetor(NUM, 0);
+	
+	time_t tempo_inicial;
+     
+	bubble_sort(vetor_elementos, NUM);
+
+	imprime_vetor(vetor_elementos, NUM);
+
+	time_t tempo_final;
+
+	printf("Tempo de execução: %ld", (tempo_final-tempo_inicial));
+
 }
